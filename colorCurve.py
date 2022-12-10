3f6f3e95618e7e2ec955a66df0a2ff4f2a3bc7e8
RFerlin <10963428@uvu.edu>
2022-12-09 22:15:51 -0700

    Added colorCurve.py

---
+++ colorCurve.py
@@ -0,0 +1,10 @@
+import maya.cmds as cmds
+
+cmds.setAttr("curveShape1.overrideEnabled", 1)
+cmds.setAttr("curveShape1.overrideColor", 17)
+#color =
+#def ChangeColor(color)
+#if color == 'yellow':
+#    color = 17
+#if color > 31:
+#    color = 0
